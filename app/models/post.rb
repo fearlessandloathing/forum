54bb86713175b844ca18e5e3eedb6208b0872a98
Andrew Amis <atamiser@gmail.com>
2013-10-13 13:56:59 -0400

    Posts now touch forum threads, forum threads now shows last updated in words
    Now, when posts are created/changed, their threads have their updated_at
    changed as well. Also, the thread index how shows when the threads were
    last modified using time_ago_in_words()

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   resourcify
   belongs_to :user
-  belongs_to :forum_thread
+  belongs_to :forum_thread, touch: true
   has_many :complaints, as: :complainable
 
   validates_presence_of :body
