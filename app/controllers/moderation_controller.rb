52cf56b016b60fd0f437bf54a5480eb59150915b
Andrew Amis <atamiser@gmail.com>
2014-10-24 03:07:59 -0700

    Added the beginning of a moderation UI, at /moderation/index

--- app/controllers/moderation_controller.rb
+++ app/controllers/moderation_controller.rb
@@ -1,5 +1,14 @@
 class ModerationController < ApplicationController
+  before_filter :check_admin
   def index
     @complainables = Complaint.all.group_by(&:complainable)
+  end
+
+  private
+  
+  def check_admin
+    if !current_user.has_role? :admin
+      redirect_to :root, :flash => { :error => "Unauthorized" }
+    end
   end
 end
